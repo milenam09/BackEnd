8e7f6e9f67cd65635cfb9b9fa9f88c8d5091f950
milena mares <maresmilena03@gmail.com>
2025-12-03 16:46:35 -0300

    novos

---
+++ MVC/DevConnect/Controllers/UusuarioController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevConnect.Controllers
+{
+    [Route("[controller]")]
+    public class UusuarioController : Controller
+    {
+        private readonly ILogger<UusuarioController> _logger;
+
+        public UusuarioController(ILogger<UusuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
